18e77cbced56be7e733358ea4e63886c9b9317c6
braenyve <braenyve@students.zhaw.ch>
2024-05-10 16:01:16 +0200

    chore: remove additional program

--- Grade_Calculator/Grade_Calculator_UI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Grade_Calculator_UI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
