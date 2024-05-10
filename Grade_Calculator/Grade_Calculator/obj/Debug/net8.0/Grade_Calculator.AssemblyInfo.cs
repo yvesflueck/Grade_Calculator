d200428b812197239f8731cab493e7640365334c
braenyve <braenyve@students.zhaw.ch>
2024-05-10 15:59:35 +0200

    chore: add bootstrap

--- Grade_Calculator/Grade_Calculator/obj/Debug/net8.0/Grade_Calculator.AssemblyInfo.cs
+++ Grade_Calculator/Grade_Calculator/obj/Debug/net8.0/Grade_Calculator.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grade_Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+658dec6d2decf31aa6349fb7e10c5df46a1d3ec2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grade_Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grade_Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
