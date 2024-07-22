ff7b7bcf32e729814d5e0ad3c92c3ccbbc696bf9
braenyve <braenyve@students.zhaw.ch>
2024-07-22 20:11:50 +0200

    cleanup project

--- Grade_Calculator/Grade_Calculator/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Grade_Calculator.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
